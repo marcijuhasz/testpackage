d266a608dcf3889a9b1f06361097a342ba8eee63
Marci <60556297+marcijuhasz@users.noreply.github.com>
2023-01-20 17:01:25 +0100

    first commit

---
+++ SZTF1HF0001_BQY4FV.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SZTF1HF0001_BQY4FV
+{
+    class SZTF1HF0001_BQY4FV
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            long divsum = 0;
+
+            for (int i = 1; i <= n; ++i)
+            {
+                divsum += (n / i) * i;
+            }
+
+            Console.WriteLine("1-" + n + "-ig a számok osztóinak összege: " + divsum + ".");
+
+            Console.ReadLine();
+
+
+        }
+
+
+    }
+}
